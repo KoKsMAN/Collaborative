75bd4a5e6446eff4bbbfa421b1b9cdb2c911ba1f
KoKsMAN <swiniak700@gmail.com>
2015-07-31 14:30:48 +0200

    Updated to version v.0.2.0

--- myscript.js
+++ myscript.js
@@ -2,81 +2,103 @@
 var player = {
   tech: 0,
   energy: 0,
+  achievements: {
+     first: 0,
+     second: 0,
+     third: 0,
+     fourth: 0,
+     fifth: 0,
+  }
 };
+
 //initialize tech building data
 var engineer = {
   name: "Engineer",
   owned: 0,
   cost: 15,
+  nextC: 15,
   generates: 1
 };
 var android = {
   name: "Android",
   owned: 0,
   cost: 100,
+  nextC: 100,
   generates: 2
 };
 var robot = {
   name: "Robot",
   owned: 0,
   cost: 500,
+  nextC: 500,
   generates: 4
 };
 var resLab = {
   name: "Research Lab",
   owned: 0,
   cost: 3000,
+  nextC: 3000,
   generates: 10
 };
 var resFac = {
   name: "Research Facility",
   owned: 0,
   cost: 10000,
+  nextC: 10000,
   generates: 40
 };
 var roboticsFact = {
   name: "Robotics Factory",
   owned: 0,
   cost: 40000,
+  nextC: 40000,
   generates: 100
 };
 var cyberLab = {
   name: "Cybernetics Lab",
   owned: 0,
   cost: 200000,
+  nextC: 200000,
   generates: 400
 };
 //initialize energy building data
 var battPack = {
   name: "Battery Pack",
   owned: 0,
-  cost: 5,
+  cost: 15,
+  nextC: 15,
   generates: 1
 };
 var pwrGen = {
   name: "Power Generator",
   owned: 0,
-  cost: 5,
+  cost: 15,
+  nextC: 15,
   generates: 1
 };
 var genRoom = {
   name: "Generator Room",
   owned: 0,
-  cost: 5,
+  cost: 15,
+  nextC: 15,
   generates: 1
 };
 var solPan = {
   name: "Solar Panels",
   owned: 0,
-  cost: 5,
+  cost: 15,
+  nextC: 15,
   generates: 1
 };
 var solPanFarm = {
   name: "Solar Panel Farm",
   owned: 0,
-  cost: 5,
+  cost: 15,
+  nextC: 15,
   generates: 1
 };
+
+
 
 //game save
 function gameSave(){
@@ -110,19 +132,25 @@ function gameLoad(){
   var savegame11 = JSON.parse(localStorage.getItem("solPan"));
   var savegame12 = JSON.parse(localStorage.getItem("solPanFarm"));
 
+  //Resources
   if (typeof savegame.tech !== "undefined") player.tech = savegame.tech;
   if (typeof savegame.energy !== "undefined") player.energy = savegame.energy;
+  //Units Tech
   if (typeof savegame2.owned !== "undefined") engineer.owned = savegame2.owned;
   if (typeof savegame3.owned !== "undefined") android.owned = savegame3.owned;
   if (typeof savegame4.owned !== "undefined") robot.owned = savegame4.owned;
   if (typeof savegame5.owned !== "undefined") resLab.owned = savegame5.owned;
   if (typeof savegame6.owned !== "undefined") resFac.owned = savegame6.owned;
   if (typeof savegame7.owned !== "undefined") roboticsFact.owned = savegame7.owned;
+  //Units Energy
   if (typeof savegame8.owned !== "undefined") battPack.owned = savegame8.owned;
   if (typeof savegame9.owned !== "undefined") pwrGen.owned = savegame9.owned;
   if (typeof savegame10.owned !== "undefined") genRoom.owned = savegame10.owned;
   if (typeof savegame11.owned !== "undefined") solPan.owned = savegame11.owned;
   if (typeof savegame12.owned !== "undefined") solPanFarm.owned = savegame12.owned;
+  //Achievements
+  if (typeof savegame.achievements !== "undefined") player.achievements = savegame.achievements;
+
 }
 
 //delete save
@@ -142,39 +170,65 @@ function deleteSave(){
 
   location.reload();
 }
-
-
+var nextCost;
 //Tech Buy
 function techBuy(amount, building, id, arrayPlace){
-  var unitCost = (amount * Math.floor(building.cost * Math.pow(1.15,building.owned)));
-  if (unitCost <= player.tech){
+  building.nextC = (amount * Math.floor(building.cost * Math.pow(1.15,building.owned)));
+  if (building.nextC <= player.tech){
     building.owned += amount;
-    player.tech -= unitCost;
+    player.tech -= building.nextC;
     document.getElementById('Tech').innerHTML = player.tech;
 
-    console.log(unitCost);
 }
   else {
       console.log("Not enough resources");
   }
-  document.getElementsByClassName('Cost')[arrayPlace].innerHTML = unitCost;
 }
 //Energy Buy
 function energyBuy(amount, building, id, arrayPlace){
-  var unitCost = (amount * Math.floor(building.cost * Math.pow(1.15,building.owned)));
-  if (unitCost <= player.energy){
+  building.nextC = (amount * Math.floor(building.cost * Math.pow(1.15,building.owned)));
+  if (building.nextC <= player.energy){
     building.owned += amount;
-    player.energy -= unitCost;
+    player.energy -= building.nextC;
     document.getElementById("Energy").innerHTML = player.energy;
-
-    console.log(unitCost);
 }
   else {
       console.log("Not enough resources");
   }
-    document.getElementsByClassName('Cost')[arrayPlace].innerHTML = unitCost;
+
 }
 
+function prettify(input){
+    var output = Math.round(input * 1000000)/1000000;
+	return output;
+}
+
+//achievements
+
+
+
+//function buyAllTech(building, arrayPlace){
+//  var total = Math.floor(player.tech / building.nextC);
+//  console.log(building.nextC);
+//  building.nextC = ( Math.floor(building.cost * Math.pow(1.15,building.owned)));
+//    if (total >= 1){
+//    building.owned += total;
+//    player.tech -= total * building.nextC;
+//    document.getElementsByClassName('Cost')[arrayPlace].innerHTML = building.nextC;
+//  }
+//}
+
+//function buyAllEnergy(building, arrayPlace){
+//  var total = Math.floor(player.energy / building.nextC);
+//  console.log(building.nextC);
+//  building.nextC = ( Math.floor(building.cost * Math.pow(1.15,building.owned)));
+//    if (total >= 1){
+//    building.owned += total;
+//    player.energy -= total * building.nextC;
+//    document.getElementById('Energy').innerHTML = player.energy;
+//    document.getElementsByClassName('Cost')[arrayPlace].innerHTML = building.nextC;
+//  }
+//}
 
 function updateTotals(){
   player.tech += (engineer.owned * engineer.generates);
@@ -191,26 +245,25 @@ function updateTotals(){
   player.energy += (solPan.owned * solPan.generates);
   player.energy += (solPanFarm.owned * solPanFarm.generates);
 
-  // Tech
-  document.getElementById('Engineers').innerHTML = engineer.owned;
-  document.getElementById('Androids').innerHTML = android.owned;
-  document.getElementById('Robot').innerHTML = robot.owned;
-  document.getElementById('resLab').innerHTML = resLab.owned;
-  document.getElementById('resFac').innerHTML = resFac.owned;
-  document.getElementById('roboticsFact').innerHTML = roboticsFact.owned;
-  document.getElementById('cyberLab').innerHTML = cyberLab.owned;
-  // Energy
-  document.getElementById('battPack').innerHTML = battPack.owned;
-  document.getElementById('pwrGen').innerHTML = pwrGen.owned;
-  document.getElementById('genRoom').innerHTML = genRoom.owned;
-  document.getElementById('solPan').innerHTML = solPan.owned;
-  document.getElementById('solPanFarm').innerHTML = solPanFarm.owned;
-  //Cash
-  document.getElementById('Tech').innerHTML = player.tech;
-  //document.getElementsByClassName('Cost')[0].innerHTML = globalCost;
-  //document.getElementsByClassName('Cost')[1].innerHTML = globalCost;
-  //document.getElementsByClassName('Cost')[2].innerHTML = globalCost;
-  //document.getElementsByClassName('Cost')[3].innerHTML = globalCost;
+}
+
+function checkAchievements (){
+  if (engineer.owned === 1 && player.achievements.first !== 1 ){
+    player.achievements.first = 1;
+    alert("Way to get started !");
+
+  }
+  if (player.achievements.first !== 0){
+    document.getElementById("first").className = "unlocked";
+  }
+
+  if (player.tech === 30 && player.achievements.second !== 1){
+    player.achievements.second = 1;
+    alert("Keep it up !");
+  }
+    if (player.achievements.second !== 0){
+    document.getElementById("second").className = "unlocked";
+  }
 }
 
 function getResource(player, resource, amount, id) {
@@ -225,6 +278,43 @@ window.setInterval(function() {
     gameSave();
     getResource(player, 'tech' , 1, "Tech");
     getResource(player, 'energy' , 1, "Energy");
-    console.log(player.tech);
+    checkAchievements ();
 
 }, 1000);
+
+window.setInterval(function() {
+    console.log("I'm working Too");
+        console.log(player.achievements.first);
+    // Tech
+    document.getElementById('Engineers').innerHTML = engineer.owned;
+    document.getElementById('Androids').innerHTML = android.owned;
+    document.getElementById('Robot').innerHTML = robot.owned;
+    document.getElementById('resLab').innerHTML = resLab.owned;
+    document.getElementById('resFac').innerHTML = resFac.owned;
+    document.getElementById('roboticsFact').innerHTML = roboticsFact.owned;
+    document.getElementById('cyberLab').innerHTML = cyberLab.owned;
+    // Energy
+    document.getElementById('battPack').innerHTML = battPack.owned;
+    document.getElementById('pwrGen').innerHTML = pwrGen.owned;
+    document.getElementById('genRoom').innerHTML = genRoom.owned;
+    document.getElementById('solPan').innerHTML = solPan.owned;
+    document.getElementById('solPanFarm').innerHTML = solPanFarm.owned;
+    //Cash
+    document.getElementById('Tech').innerHTML = player.tech;
+    document.getElementById('Energy').innerHTML = player.energy;
+    //Cost Tech
+    document.getElementsByClassName('Cost')[0].innerHTML = engineer.nextC;
+    document.getElementsByClassName('Cost')[1].innerHTML = android.nextC;
+    document.getElementsByClassName('Cost')[2].innerHTML = robot.nextC;
+    document.getElementsByClassName('Cost')[3].innerHTML = resLab.nextC;
+    document.getElementsByClassName('Cost')[4].innerHTML = resFac.nextC;
+    document.getElementsByClassName('Cost')[5].innerHTML = roboticsFact.nextC;
+    document.getElementsByClassName('Cost')[6].innerHTML = cyberLab.nextC;
+    //Cost Energy
+    document.getElementsByClassName('Cost')[7].innerHTML = battPack.nextC;
+    document.getElementsByClassName('Cost')[8].innerHTML = pwrGen.nextC;
+    document.getElementsByClassName('Cost')[9].innerHTML = genRoom.nextC;
+    document.getElementsByClassName('Cost')[10].innerHTML = solPan.nextC;
+    document.getElementsByClassName('Cost')[11].innerHTML = solPanFarm.nextC;
+
+}, 100);
