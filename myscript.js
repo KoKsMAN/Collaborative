536edc6f26118fc1dfdb4797e338a7648b3d40c5
KoKsMAN <swiniak700@gmail.com>
2015-08-31 16:07:21 +0200

    Changes to the buy 5 button

--- myscript.js
+++ myscript.js
@@ -1,6 +1,6 @@
 //initialize player data
 var player = {
-  tech: 15,
+  tech: 1000,
   energy: 0,
   engineer: {
     name: "Engineer",
@@ -269,6 +269,9 @@ function deleteSave(){
   localStorage.removeItem('player');
   location.reload();
 }
+
+
+
 //Tech Buy
 function techBuy(amount, building, id, arrayPlace){
   building.nextC = ((amount * Math.floor(building.cost * Math.pow(1.15,building.owned))));
@@ -294,6 +297,24 @@ function energyBuy(amount, building, id, arrayPlace){
       console.log("Not enough resources");
   }
 }
+
+function energyBuyFive(building, amountToPurchase){ //Now you send two values, building and amountToPurchase.
+    var exponentialIncrease = 1.15; //**Easier to put this up here, so you only have to change one number if you want to rebalance this.
+    var nextCost = 0; //**There's no reason to store nextC in the player object. You'll want to calculate this out when you check it, then have it be disposed
+    nextCost = Math.floor((building.cost * Math.pow(exponentialIncrease, building.owned)) * ((Math.pow(exponentialIncrease, amountToPurchase) - 1) / (exponentialIncrease - 1)));
+    //now determines cost of any number of buildings
+    if (nextCost <= player.tech){ //checks if player can afford the purchase
+        building.owned += amountToPurchase; //increments the amount of buildings by amount to purchase
+        player.tech -= nextCost; // removes the resource used to purchase the building
+        document.getElementById("Tech").innerHTML = suffixy(player.tech, 2); // updates the amount of resource on the screen.
+        building.nextC = ((Math.floor(building.cost * Math.pow(1.15,building.owned))));
+        console.log(nextCost);
+    }
+    else {
+        console.log("Not enough resources");
+    }
+}
+
 function buyUpgrade(techcost, energycost, up, amount){
     if(techcost <= player.tech && energycost <= player.energy){
       player.tech -= techcost;
@@ -395,7 +416,7 @@ window.setInterval(function() {
   console.log(player.engineer.nextC);
   updateTotals();
   gameSave();
-  getResource(player, 'tech' , 1);
+  //getResource(player, 'tech' , 1);
   getResource(player, 'energy' , 1);
   // hideElements();
   //save_game();
@@ -422,13 +443,13 @@ window.setInterval(function() {
     document.getElementById('Tech').innerHTML = prettify(player.tech);
   }
   else {
-    document.getElementById('Tech').innerHTML = suffixy(player.tech, 2);
+    document.getElementById('Tech').innerHTML = suffixy(player.tech, 1);
   }
   if (player.energy <= 1000){
     document.getElementById('Energy').innerHTML = player.energy;
   }
   else {
-    document.getElementById('Energy').innerHTML = suffixy(player.energy, 2);
+    document.getElementById('Energy').innerHTML = suffixy(player.energy, 1);
   }
   //checkAchievements ();
   checkUpgrades();
